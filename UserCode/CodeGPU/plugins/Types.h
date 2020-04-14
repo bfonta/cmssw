37e3a21d2cf2e5b83fd6c73c83a6b62246638124
bfontana <bruno.alves@cern.ch>
2020-04-14 12:19:10 +0200

    remove needless file

--- UserCode/CodeGPU/plugins/Types.h
+++
@@ -1,8 +0,0 @@
-#ifndef RecoLocalCalo_HGCalRecProducers_Types_h
-#define RecoLocalCalo_HGCalRecProducers_Types_h
-
-#include <vector>
-
-//enum FlagsGPU {kGood=0, kPoorReco, kOutOfTime, kFaultyHardware, kNoisy, kPoorCalib, kSaturated, kDead, kKilled, kWeird, kDiWerid, kUnknown};
-
-#endif //RecoLocalCalo_HGCalRecProducers_Types_h
