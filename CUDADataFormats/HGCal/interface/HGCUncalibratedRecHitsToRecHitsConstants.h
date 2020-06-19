c2ee2324fec4f5783a9f57304576bc1e9a6c69cf
Bruno Alves <bruno.alves@cern.ch>
2020-06-19 14:21:09 +0200

    x and y position calculation in kernel

--- CUDADataFormats/HGCal/interface/HGCUncalibratedRecHitsToRecHitsConstants.h
+++ CUDADataFormats/HGCal/interface/HGCUncalibratedRecHitsToRecHitsConstants.h
@@ -6,19 +6,19 @@
 //maximum sizes for SoA's arrays holding configuration data ("constants")
 namespace maxsizes_constants {
   //EE
-  constexpr size_t ee_fCPerMIP = 6; //number of elements pointed by hgcEE_fCPerMIP_
-  constexpr size_t ee_cce = 6; //number of elements posize_ted by hgcEE_cce_
-  constexpr size_t ee_noise_fC = 6; //number of elements posize_ted by hgcEE_noise_fC_
-  constexpr size_t ee_rcorr = 6; //number of elements posize_ted by rcorr_
-  constexpr size_t ee_weights = 53; //number of elements posize_ted by weights_
+  constexpr size_t ee_fCPerMIP = 3; //number of elements pointed by hgcEE_fCPerMIP_
+  constexpr size_t ee_cce = 3; //number of elements pointed by hgcEE_cce_
+  constexpr size_t ee_noise_fC = 3; //number of elements pointed by hgcEE_noise_fC_
+  constexpr size_t ee_rcorr = 3; //number of elements pointed by rcorr_
+  constexpr size_t ee_weights = 51; //number of elements posize_ted by weights_
   //HEF
-  constexpr size_t hef_fCPerMIP = 6; //number of elements pointed by hgcEE_fCPerMIP_
-  constexpr size_t hef_cce = 6; //number of elements posize_ted by hgcEE_cce_
-  constexpr size_t hef_noise_fC = 6; //number of elements posize_ted by hgcEE_noise_fC_
-  constexpr size_t hef_rcorr = 6; //number of elements posize_ted by rcorr_
-  constexpr size_t hef_weights = 53; //number of elements posize_ted by weights_
+  constexpr size_t hef_fCPerMIP = 3; //number of elements pointed by hgcEE_fCPerMIP_
+  constexpr size_t hef_cce = 3; //number of elements pointed by hgcEE_cce_
+  constexpr size_t hef_noise_fC = 3; //number of elements pointed by hgcEE_noise_fC_
+  constexpr size_t hef_rcorr = 3; //number of elements pointed by rcorr_
+  constexpr size_t hef_weights = 51; //number of elements pointed by weights_
   //HEB
-  constexpr size_t heb_weights = 53; //number of elements posize_ted by weights_
+  constexpr size_t heb_weights = 51; //number of elements pointed by weights_
 }
 
 class HGCConstantVectorData {
@@ -60,6 +60,7 @@ class HGChefUncalibratedRecHitConstantData {
   float xmax_;         //used for computing the time resolution error
   float aterm_;        //used for computing the time resolution error
   float cterm_;        //used for computing the time resolution error
+  uint32_t layerOffset_;  //layer offset relative to layer#1 of the EE subsetector
 };
 
 class HGChebUncalibratedRecHitConstantData {
@@ -69,6 +70,7 @@ class HGChebUncalibratedRecHitConstantData {
   double keV2DIGI_;   //energy to femto coloumb conversion: 1000 eV/3.62 (eV per e) / 6.24150934e3 (e per fC)
   double uncalib2GeV_; //sets the ADC; obtained by dividing 1e-6 by hgcHEB_keV2DIGI_
   double noise_MIP_;  //noise
+  uint32_t layerOffset_;  //layer offset relative to layer#1 of the EE subsetector
 };
 
 #endif
