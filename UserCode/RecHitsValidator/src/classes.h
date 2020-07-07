98878334ba7f08490571aeb0d567c860addfaaf2
Bruno Alves <bruno.alves@cern.ch>
2020-07-07 14:05:44 +0200

    commit for Pedro

---
+++ UserCode/RecHitsValidator/src/classes.h
@@ -0,0 +1,7 @@
+#include "UserCode/RecHitsValidator/interface/validHit.h"
+#include "DataFormats/Common/interface/AssociationVector.h"
+#include "DataFormats/Common/interface/AssociationMap.h"
+
+validHit vh;
+std::vector<validHit> vvh;
+edm::Wrapper<std::vector<validHit> > wvvh;
