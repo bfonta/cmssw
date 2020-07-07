cbb03fcb755704484487a49875155a4d53471bef
Bruno Alves <bruno.alves@cern.ch>
2020-07-07 16:08:05 +0200

    -

--- UserCode/RecHitsValidator/src/classes.h
+++ UserCode/RecHitsValidator/src/classes.h
@@ -1,3 +1,12 @@
+#ifdef __CINT_
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class validHit+;
+#pragma link C++ class vector<validHit>+;
+#endif /* __CINT__ */
+
 #include "UserCode/RecHitsValidator/interface/validHit.h"
 #include "DataFormats/Common/interface/AssociationVector.h"
 #include "DataFormats/Common/interface/AssociationMap.h"
