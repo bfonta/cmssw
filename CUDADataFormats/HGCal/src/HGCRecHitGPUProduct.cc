10d23034de1e1666201a2e64d4883e32371c65d3
Bruno Alves <balves16@hotmail.com>
2021-02-16 11:40:09 +0100

    add memory layout in GPU product class

--- CUDADataFormats/HGCal/src/HGCRecHitGPUProduct.cc
+++ CUDADataFormats/HGCal/src/HGCRecHitGPUProduct.cc
@@ -1,12 +1,26 @@
 #include "CUDADataFormats/HGCal/interface/HGCRecHitGPUProduct.h"
 
-HGCRecHitGPUProduct::HGCRecHitGPUProduct(const uint32_t& nhits, const cudaStream_t& stream) : nhits_(nhits) {
-  stride_ = ((nhits - 1) / 32 + 1) * 32;  //align to warp boundary
-
-  std::vector<int> sizes = { memory::npointers::float_hgcrechits_soa * sizeof(float),
-                             memory::npointers::uint32_hgcrechits_soa * sizeof(uint32_t),
-                             memory::npointers::uint8_hgcrechits_soa * sizeof(uint8_t) };
+HGCRecHitGPUProduct::HGCRecHitGPUProduct(uint32_t nhits, const cudaStream_t& stream) : nhits_(nhits) {
+  constexpr std::array<int,3> sizes = {{ memory::npointers::float_hgcrechits_soa  * sizeof(float),
+					 memory::npointers::uint32_hgcrechits_soa * sizeof(uint32_t),
+					 memory::npointers::uint8_hgcrechits_soa  * sizeof(uint8_t) }};
   size_tot_ = std::accumulate(sizes.begin(), sizes.end(), 0);
-  ptr_ = cms::cuda::make_device_unique<std::byte[]>(stride_ * size_tot_, stream);
-  assert(sizes.begin() + memory::npointers::ntypes_hgcrechits_soa == sizes.end());
+  pad_ = ((nhits - 1) / 32 + 1) * 32;  //align to warp boundary (assumption: warpSize = 32)
+  ptr_ = cms::cuda::make_device_unique<std::byte[]>(pad_ * size_tot_, stream);
+  static_assert(sizes.begin() + memory::npointers::ntypes_hgcrechits_soa == sizes.end());
+
+  defineSoAMemoryLayout_();
+}
+
+void HGCRecHitGPUProduct::defineSoAMemoryLayout_() {
+  soa_.energy_    = reinterpret_cast<float*>(ptr_.get());
+  soa_.time_      = soa_.energy_ + pad_;
+  soa_.timeError_ = soa_.time_ + pad_;
+  soa_.id_        = reinterpret_cast<uint32_t*>(soa_.timeError_ + pad_);
+  soa_.flagBits_  = soa_.id_ + pad_;
+  soa_.son_       = reinterpret_cast<uint8_t*>(soa_.flagBits_ + pad_);
+
+  soa_.nbytes_ = size_tot_;
+  soa_.nhits_  = nhits_;
+  soa_.pad_    = pad_;
 }
--- RecoLocalCalo/HGCalRecProducers/plugins/HeterogeneousHGCRecHitMemAllocations.cc
+++ RecoLocalCalo/HGCalRecProducers/plugins/HeterogeneousHGCRecHitMemAllocations.cc
@@ -3,28 +3,28 @@
 namespace memory {
   namespace allocation {
     cms::cuda::device::unique_ptr<std::byte[]> uncalibRecHitDevice(const uint32_t& nhits,
-                                                                   const uint32_t& stride,
+                                                                   const uint32_t& pad,
                                                                    HGCUncalibratedRecHitSoA& soa,
                                                                    const cudaStream_t& stream) {
       std::vector<int> sizes = {npointers::float_hgcuncalibrechits_soa * sizeof(float),
                                 npointers::uint32_hgcuncalibrechits_soa * sizeof(uint32_t)};
       int size_tot = std::accumulate(sizes.begin(), sizes.end(), 0);
       cms::cuda::device::unique_ptr<std::byte[]> mem =
-          cms::cuda::make_device_unique<std::byte[]>(stride * size_tot, stream);
+          cms::cuda::make_device_unique<std::byte[]>(pad * size_tot, stream);
 
       soa.amplitude_ = reinterpret_cast<float*>(mem.get());
-      soa.pedestal_ = soa.amplitude_ + stride;
-      soa.jitter_ = soa.pedestal_ + stride;
-      soa.chi2_ = soa.jitter_ + stride;
-      soa.OOTamplitude_ = soa.chi2_ + stride;
-      soa.OOTchi2_ = soa.OOTamplitude_ + stride;
-      soa.flags_ = reinterpret_cast<uint32_t*>(soa.OOTchi2_ + stride);
-      soa.aux_ = soa.flags_ + stride;
-      soa.id_ = soa.aux_ + stride;
+      soa.pedestal_ = soa.amplitude_ + pad;
+      soa.jitter_ = soa.pedestal_ + pad;
+      soa.chi2_ = soa.jitter_ + pad;
+      soa.OOTamplitude_ = soa.chi2_ + pad;
+      soa.OOTchi2_ = soa.OOTamplitude_ + pad;
+      soa.flags_ = reinterpret_cast<uint32_t*>(soa.OOTchi2_ + pad);
+      soa.aux_ = soa.flags_ + pad;
+      soa.id_ = soa.aux_ + pad;
 
       soa.nbytes_ = size_tot;
       soa.nhits_ = nhits;
-      soa.stride_ = stride;
+      soa.pad_ = pad;
 
       assert(sizes.begin() + npointers::ntypes_hgcuncalibrechits_soa == sizes.end());
 
@@ -32,30 +32,30 @@ namespace memory {
     }
 
     cms::cuda::host::unique_ptr<std::byte[]> uncalibRecHitHost(const uint32_t& nhits,
-							       const uint32_t& stride,
+							       const uint32_t& pad,
                                                                HGCUncalibratedRecHitSoA& soa,
                                                                const cudaStream_t& stream) {
       std::vector<int> sizes = {npointers::float_hgcuncalibrechits_soa * sizeof(float),
                                 npointers::uint32_hgcuncalibrechits_soa * sizeof(uint32_t)};
       int size_tot = std::accumulate(sizes.begin(), sizes.end(), 0);
       cms::cuda::host::unique_ptr<std::byte[]> mem =
-          cms::cuda::make_host_unique<std::byte[]>(stride * size_tot, stream);
+          cms::cuda::make_host_unique<std::byte[]>(pad * size_tot, stream);
 
       soa.amplitude_ = reinterpret_cast<float*>(mem.get());
-      soa.pedestal_ = soa.amplitude_ + stride;
-      soa.jitter_ = soa.pedestal_ + stride;
-      soa.chi2_ = soa.jitter_ + stride;
-      soa.OOTamplitude_ = soa.chi2_ + stride;
-      soa.OOTchi2_ = soa.OOTamplitude_ + stride;
-      soa.flags_ = reinterpret_cast<uint32_t*>(soa.OOTchi2_ + stride);
-      soa.aux_ = soa.flags_ + stride;
-      soa.id_ = soa.aux_ + stride;
-      soa.aux_ = soa.flags_ + stride;
-      soa.id_ = soa.aux_ + stride;
+      soa.pedestal_ = soa.amplitude_ + pad;
+      soa.jitter_ = soa.pedestal_ + pad;
+      soa.chi2_ = soa.jitter_ + pad;
+      soa.OOTamplitude_ = soa.chi2_ + pad;
+      soa.OOTchi2_ = soa.OOTamplitude_ + pad;
+      soa.flags_ = reinterpret_cast<uint32_t*>(soa.OOTchi2_ + pad);
+      soa.aux_ = soa.flags_ + pad;
+      soa.id_ = soa.aux_ + pad;
+      soa.aux_ = soa.flags_ + pad;
+      soa.id_ = soa.aux_ + pad;
 
       soa.nbytes_ = size_tot;
       soa.nhits_ = nhits;
-      soa.stride_ = stride;
+      soa.pad_ = pad;
 
       assert(sizes.begin() + npointers::ntypes_hgcuncalibrechits_soa == sizes.end());
 
@@ -63,7 +63,7 @@ namespace memory {
     }
 
     cms::cuda::host::unique_ptr<std::byte[]> calibRecHitHost(const uint32_t& nhits,
-                                                             const uint32_t& stride,
+                                                             const uint32_t& pad,
                                                              HGCRecHitSoA& soa,
                                                              const cudaStream_t& stream) {
       std::vector<int> sizes = {npointers::float_hgcrechits_soa * sizeof(float),
@@ -71,35 +71,21 @@ namespace memory {
                                 npointers::uint8_hgcrechits_soa * sizeof(uint8_t)};
       int size_tot = std::accumulate(sizes.begin(), sizes.end(), 0);
       cms::cuda::host::unique_ptr<std::byte[]> mem =
-          cms::cuda::make_host_unique<std::byte[]>(stride * size_tot, stream);
+          cms::cuda::make_host_unique<std::byte[]>(pad * size_tot, stream);
       soa.energy_ = reinterpret_cast<float*>(mem.get());
-      soa.time_ = soa.energy_ + stride;
-      soa.timeError_ = soa.time_ + stride;
-      soa.id_ = reinterpret_cast<uint32_t*>(soa.timeError_ + stride);
-      soa.flagBits_ = soa.id_ + stride;
-      soa.son_ = reinterpret_cast<uint8_t*>(soa.flagBits_ + stride);
+      soa.time_ = soa.energy_ + pad;
+      soa.timeError_ = soa.time_ + pad;
+      soa.id_ = reinterpret_cast<uint32_t*>(soa.timeError_ + pad);
+      soa.flagBits_ = soa.id_ + pad;
+      soa.son_ = reinterpret_cast<uint8_t*>(soa.flagBits_ + pad);
 
       soa.nbytes_ = size_tot;
       soa.nhits_ = nhits;
-      soa.stride_ = stride;
+      soa.pad_ = pad;
 
       assert(sizes.begin() + npointers::ntypes_hgcrechits_soa == sizes.end());
 
       return mem;
-    }
-
-    void calibRecHitDevice(
-        const uint32_t& nhits, const uint32_t& stride, const uint32_t& nbytes, HGCRecHitSoA& soa, std::byte* mem) {
-      soa.energy_ = reinterpret_cast<float*>(mem);
-      soa.time_ = soa.energy_ + stride;
-      soa.timeError_ = soa.time_ + stride;
-      soa.id_ = reinterpret_cast<uint32_t*>(soa.timeError_ + stride);
-      soa.flagBits_ = soa.id_ + stride;
-      soa.son_ = reinterpret_cast<uint8_t*>(soa.flagBits_ + stride);
-
-      soa.nbytes_ = nbytes;
-      soa.nhits_ = nhits;
-      soa.stride_ = stride;
     }
   }  // namespace allocation
 }  // namespace memory
