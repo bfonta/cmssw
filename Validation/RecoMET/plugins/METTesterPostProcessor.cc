cc6aba466230c082a2849ec72c21bf46c7be4918
Bruno Alves <balves16@hotmail.com>
2025-09-26 11:40:48 +0200

    wip

--- Validation/RecoMET/plugins/METTesterPostProcessor.cc
+++ Validation/RecoMET/plugins/METTesterPostProcessor.cc
@@ -7,81 +7,79 @@ METTesterPostProcessor::~METTesterPostProcessor() {}
 
 // ------------ method called right after a run ends ------------
 void METTesterPostProcessor::dqmEndJob(DQMStore::IBooker &ibook_, DQMStore::IGetter &iget_) {
-  std::vector<std::string> subDirVec;
-  std::string RunDir;
-  if (isHLT)
-    RunDir = "HLT/JetMET/METValidation/";
-  else
-    RunDir = "JetMET/METValidation/";
+  // std::vector<std::string> subDirVec;
+  // std::string RunDir;
+  // if (isHLT)
+  //   RunDir = "HLT/JetMET/METValidation/";
+  // else
+  //   RunDir = "JetMET/METValidation/";
 
-  iget_.setCurrentFolder(RunDir);
-  met_dirs = iget_.getSubdirs();
+  // iget_.setCurrentFolder(RunDir);
+  // met_dirs = iget_.getSubdirs();
 
   // loop over met subdirectories
-  for (size_t i = 0; i < met_dirs.size(); i++) {
-    ibook_.setCurrentFolder(met_dirs[i]);
-    for (std::string bt : {"MET", "Phi"}) {  // loop over bin types
-      mMETDiffAggr[bt] = ibook_.book1D("METDiffAggr_" + bt,
-                                       "METDiffAggr_" + bt,
-                                       mNBins[bt],
-                                       std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
-      mMETRespAggr[bt] = ibook_.book1D("METRespAggr_" + bt,
-                                       "METRespAggr_" + bt,
-                                       mNBins[bt],
-                                       std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
-      mMETResolAggr[bt] = ibook_.book1D("METResolAggr_" + bt,
-                                        "METResolAggr_" + bt,
-                                        mNBins[bt],
-                                        std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
-      mMETSignAggr[bt] = ibook_.book1D("METSignAggr_" + bt,
-                                       "METSignAggr_" + bt,
-                                       mNBins[bt],
-                                       std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
-    }
-    mFillAggrHistograms(met_dirs[i], iget_);
-  }
+  // for (size_t i = 0; i < met_dirs.size(); i++) {
+  //   ibook_.setCurrentFolder(met_dirs[i]);
+  //   for (std::string bt : {"MET", "Phi"}) {  // loop over bin types
+  //     mMETDiffAggr[bt] = ibook_.book1D("METDiffAggr_" + bt,
+  //                                      "METDiffAggr_" + bt,
+  //                                      mNBins[bt],
+  //                                      std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
+  //     mMETRespAggr[bt] = ibook_.book1D("METRespAggr_" + bt,
+  //                                      "METRespAggr_" + bt,
+  //                                      mNBins[bt],
+  //                                      std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
+  //     mMETResolAggr[bt] = ibook_.book1D("METResolAggr_" + bt,
+  //                                       "METResolAggr_" + bt,
+  //                                       mNBins[bt],
+  //                                       std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
+  //     mMETSignAggr[bt] = ibook_.book1D("METSignAggr_" + bt,
+  //                                      "METSignAggr_" + bt,
+  //                                      mNBins[bt],
+  //                                      std::visit([](const auto &arr) { return arr.data(); }, mEdges[bt]));
+  //   }
+  //   mFillAggrHistograms(met_dirs[i], iget_);
+  // }
 }
 
 void METTesterPostProcessor::mFillAggrHistograms(std::string metdir, DQMStore::IGetter &iget) {
   for (std::string bt : {"MET", "Phi"}) {  // loop over bin types
-    for (unsigned idx = 0; idx < mNBins[bt]; ++idx) {
-      std::string edges =
-          METTester::binStr(mArrayIdx<float>(mEdges[bt], idx), mArrayIdx<float>(mEdges[bt], idx + 1), bt == "MET");
-      mArrayIdx<MElem *>(mMET[bt], idx) = iget.get(metdir + "/MET_" + bt + edges);
-      mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx) = iget.get(metdir + "/METDiff_GenMETTrue_" + bt + edges);
-      mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx) = iget.get(metdir + "/METRatio_GenMETTrue_" + bt + edges);
-      mArrayIdx<MElem *>(mMETDeltaPhi_GenMETTrue[bt], idx) = iget.get(metdir + "/METDeltaPhi_GenMETTrue_" + bt + edges);
-      std::cout << idx << ": " << edges << std::endl;
-    }
+    // for (unsigned idx = 0; idx < mNBins[bt]; ++idx) {
+    //   std::string edges =
+    //       METTester::binStr(mArrayIdx<float>(mEdges[bt], idx), mArrayIdx<float>(mEdges[bt], idx + 1), bt == "MET");
+    //   mArrayIdx<MElem *>(mMET[bt], idx) = iget.get(metdir + "/MET_" + bt + edges);
+    //   mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx) = iget.get(metdir + "/METDiff_GenMETTrue_" + bt + edges);
+    //   mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx) = iget.get(metdir + "/METRatio_GenMETTrue_" + bt + edges);
+    //   mArrayIdx<MElem *>(mMETDeltaPhi_GenMETTrue[bt], idx) = iget.get(metdir + "/METDeltaPhi_GenMETTrue_" + bt + edges);
+    // }
 
     // check one object, if it exists, then the remaining ME's exists too
     // for genmet none of these ME's are filled
-    if (mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], 0) &&
-        mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], 0)->getRootObject()) {
-      for (unsigned idx = 0; idx < mNBins[bt]; ++idx) {
-        mMETDiffAggr[bt]->setBinContent(idx + 1, mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx)->getMean());
-        mMETDiffAggr[bt]->setBinError(idx + 1, mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx)->getRMS());
+    // if (mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], 0) &&
+    //     mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], 0)->getRootObject()) {
+    //   for (unsigned idx = 0; idx < mNBins[bt]; ++idx) {
+    //     mMETDiffAggr[bt]->setBinContent(idx + 1, mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx)->getMean());
+    //     mMETDiffAggr[bt]->setBinError(idx + 1, mArrayIdx<MElem *>(mMETDiff_GenMETTrue[bt], idx)->getRMS());
 
-        float ratioMean = mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx)->getMean();
-        float ratioRMS = mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx)->getRMS();
-        mMETRespAggr[bt]->setBinContent(idx + 1, ratioMean);
-        mMETRespAggr[bt]->setBinError(idx + 1, ratioRMS);
+    //     float ratioMean = mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx)->getMean();
+    //     float ratioRMS = mArrayIdx<MElem *>(mMETRatio_GenMETTrue[bt], idx)->getRMS();
+    //     mMETRespAggr[bt]->setBinContent(idx + 1, ratioMean);
+    //     mMETRespAggr[bt]->setBinError(idx + 1, ratioRMS);
 
-        float metMean = mArrayIdx<MElem *>(mMET[bt], idx)->getMean();
-        float metRMS = mArrayIdx<MElem *>(mMET[bt], idx)->getRMS();
-        float resolError = mArrayIdx<MElem *>(mMET[bt], idx)->getRMSError();
-        mMETResolAggr[bt]->setBinContent(idx + 1, metRMS);
-        mMETResolAggr[bt]->setBinError(idx + 1, resolError);
+    //     float metMean = mArrayIdx<MElem *>(mMET[bt], idx)->getMean();
+    //     float metRMS = mArrayIdx<MElem *>(mMET[bt], idx)->getRMS();
+    //     float resolError = mArrayIdx<MElem *>(mMET[bt], idx)->getRMSError();
+    //     mMETResolAggr[bt]->setBinContent(idx + 1, metRMS);
+    //     mMETResolAggr[bt]->setBinError(idx + 1, resolError);
 
-        std::cout << idx << ": " << metRMS << ", " << metMean << ", " << std::endl;
 
-        float significance = metMean / metRMS;
-        mMETSignAggr[bt]->setBinContent(idx + 1, significance);
-        mMETSignAggr[bt]->setBinError(idx + 1,
-                                      significance * std::sqrt((metRMS * metRMS / (metMean * metMean)) +
-                                                               (resolError * resolError / (metRMS * metRMS))));
-      }
-    }
+    //     float significance = metMean / metRMS;
+    //     mMETSignAggr[bt]->setBinContent(idx + 1, significance);
+    //     mMETSignAggr[bt]->setBinError(idx + 1,
+    //                                   significance * std::sqrt((metRMS * metRMS / (metMean * metMean)) +
+    //                                                            (resolError * resolError / (metRMS * metRMS))));
+    //   }
+    // }
   }
 }
 
